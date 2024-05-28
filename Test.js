34523554c9778b80eb94ef4537b2729a9b1554dc
Lokesh Rawte <54883157+lokeshrawte22@users.noreply.github.com>
2024-05-28 15:51:54 +0530

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,23 @@
+
+    let i= 10
+    for(i=1; i<=10; i++)
+    {
+        if(i%2 ==0)
+        {
+            console.log( i + " is even number ")
+        }
+        
+    }
+    console.log("\n")
+    for(i=1; i <=10 ; i++)
+    {
+        if(i%2==1)
+        {
+            
+            console.log( i + " is odd number ")
+        }
+    }
+
+    
+
+}
